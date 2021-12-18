f634ddc03fc8b46c285e23a2f379ff4524d0adfa
Barry Tickle <barry.tickle12@gmail.com>
2021-12-18 15:30:36 +0000

    React Redux basic

--- src/index.js
+++ src/index.js
@@ -2,16 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import allReducers from './reducers';
+import {createStore, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
+
+const store = createStore(
+  allReducers, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+//ACTION -> DESCRIBES WHAT YOU WANT TO DO
+
+//REDUCER -> DESCRIBE HOW YOUR ACTIONS TRANSFORM YOUR STATE INTO THE NEXT STATE
+
+//STORE -> GLOBALIZED STATE
+
+//DISPATCH -> WHERE YOU CAN EXECUTE THAT ACTION
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
